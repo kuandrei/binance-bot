d8d43676e1189cb26f8e0e7f05c47362d518607b
Andrei Kutsovskiy <andrei@websplanet.com>
2019-07-04 18:05:31 +0300

    checkStopLossOrder worker

--- app/workers/index.js
+++ app/workers/index.js
@@ -1,3 +1,6 @@
+/**
+ * @see docs/workers.md
+ */
 const Queue = require('bull');
 
 const init = () => {
@@ -8,12 +11,12 @@ const init = () => {
     const addTradePairsForAnalysisQueue = new Queue('add-trade-pairs-for-analysis', 'redis://redis:6379');
     addTradePairsForAnalysisQueue.process(__dirname + '/add-trade-pairs-for-analysis.js');
     /**
-     * @todo add description
+     * Checks if the new deal should be opened for given trade pair
      */
     const analyzeTradePairQueue = new Queue('analyze-trade-pair', 'redis://redis:6379');
     analyzeTradePairQueue.process(__dirname + '/analyze-trade-pair.js');
     /**
-     * @todo add description
+     *  Opens new deal for given trade pair. Sets new buy/sell order (depends on deal type)
      */
     const openNewDealQueue = new Queue('open-new-deal', 'redis://redis:6379');
     openNewDealQueue.process(__dirname + '/open-new-deal.js');
@@ -28,11 +31,11 @@ const init = () => {
     const addStopLossOrderQueue = new Queue('add-stop-loss-order', 'redis://redis:6379');
     addStopLossOrderQueue.process(__dirname + '/add-stop-loss-order.js');
 
-    /**
-     * @todo add description
-     */
-    const checkStopLossOrdersQueue = new Queue('check-stop-loss-orders', 'redis://redis:6379');
-    checkStopLossOrdersQueue.process(__dirname + '/check-stop-loss-orders.js');
+    // /**
+    //  * @todo add description
+    //  */
+    // const checkStopLossOrdersQueue = new Queue('check-stop-loss-orders', 'redis://redis:6379');
+    // checkStopLossOrdersQueue.process(__dirname + '/check-stop-loss-orders.js');
 
     /**
      * Adds prepare-symbol-info task for every active/trading symbol
