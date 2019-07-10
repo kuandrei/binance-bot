ac222b06aa951ed885abfa2ef0d526aa1aa022a9
kuandrei <kuandrei@gmail.com>
2019-07-10 15:38:48 +0300

    fixes

--- app/workers/replace-stop-loss-order.js
+++ app/workers/replace-stop-loss-order.js
@@ -1,6 +1,6 @@
 const debug = require('debug')('bnb:workers:replace-stop-loss-order');
 const binanceHelpers = require('./../helpers/binance');
-const {Deal} = require('./../models');
+const {Order, Deal} = require('./../models');
 const errorHandler = require('../helpers/error-handler');
 
 const Queue = require('bull');
@@ -9,13 +9,16 @@ const addStopLossOrderQueue = new Queue('add-stop-loss-order', 'redis://redis:63
  * 1. cancel existing order
  * 2. place new one by adding add-stop-loss-order task
  */
-async function replaceStopLossOrder({order, stopLossPrice}) {
+async function replaceStopLossOrder(task) {
 
     try {
-
+        const {order, stopLossPrice} = task.data;
         await binanceHelpers.cancelOrder(order.clientId, order.symbol, order.binanceOrderId);
-        order.status = 'CANCELED';
-        await order.save();
+        
+        const o = await TradePair.findByPk(order.id);
+        o.status = 'CANCELED';
+        await o.save();
+
         debug(`REPLACE STOP_LOSS_LIMIT (DEAL#${order.dealId}/ORDER#${order.id}/${order.symbol}/PREV-PRICE:${order.price}/NEW-PRICE:${stopLossPrice})`);
 
         const deal = await Deal.findByPk(order.dealId);
@@ -26,10 +29,7 @@ async function replaceStopLossOrder({order, stopLossPrice}) {
 
 
     } catch (err) {
-        errorHandler(err, {
-            order: order.toJSON(),
-            stopLossPrice
-        });
+        rrorHandler(err, task.data);
         debug(`ERROR: ${err.message}`);
     }
 
